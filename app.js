0e400d3a0be5ceb1299bfe7314b7fc65e3329a38
herrylegowo <herrylegowo19@gmail.com>
2020-06-23 01:03:25 +0800

    routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load Config
@@ -10,9 +12,20 @@ connectDB()
 
 const app = express()
 
+// logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+// handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
